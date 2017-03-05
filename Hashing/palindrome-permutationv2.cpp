525e9fdbb8ddb01968387d8bafa6aa791f751d11
Endiliey <endiliey@gmail.com>
2017-03-05 10:58:50 +0800

    Moved palindrome permuation to hash section

---
+++ Hashing/palindrome-permutationv2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+	string a = "Rats live on no evil star";
+	//string a = "Tact coa";
+	//string a = "endi toni";
+	
+	unordered_map<char, int> cntr;
+	for (auto &chr : a)
+	{
+		if (chr == ' ')
+			continue;
+		chr = tolower(chr);
+		cntr[chr]++;
+	}
+
+	bool foundMiddle = false;
+	for (auto &p : cntr)
+	{
+		if (p.second % 2 == 1)
+		{
+			if (foundMiddle)
+			{
+				cout << "False" << endl;
+				return 0;
+			}
+			foundMiddle = true;
+		}
+	}
+	cout << "True" << endl;
+	return 0;
+}
